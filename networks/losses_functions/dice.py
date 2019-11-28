fb06aac491b26348399ec3eec956130d07af3b79
Filip Barańczuk <f.baranczuk@gmail.com>
2019-11-28 17:22:40 +0100

    Fix dice score

--- networks/losses_functions/dice.py
+++ networks/losses_functions/dice.py
@@ -8,4 +8,4 @@ def dice_loss(y_true, y_pred):
     numerator = 2 * tf.reduce_sum(y_true_flat * y_pred_flat)
     denominator = tf.reduce_sum(y_true_flat + y_pred_flat)
 
-    return 1 - numerator / denominator
+    return 1. - (numerator + 1) / (denominator + 1)
