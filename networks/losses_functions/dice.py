c1e05b398b1ef36cad28da52a58c28c106280696
Filip Barańczuk <f.baranczuk@gmail.com>
2019-11-29 10:21:32 +0100

    Final dice score version

--- networks/losses_functions/dice.py
+++ networks/losses_functions/dice.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 
 
-def dice_loss(y_true, y_pred):
+def dice_loss(y_true, y_pred, smooth=1.):
     y_true_flat = tf.reshape(y_true, [-1])
     y_pred_flat = tf.reshape(y_pred, [-1])
 
-    numerator = 2 * tf.reduce_sum(y_true_flat * y_pred_flat)
+    numerator = 2. * tf.reduce_sum(y_true_flat * y_pred_flat)
     denominator = tf.reduce_sum(y_true_flat + y_pred_flat)
 
-    return 1. - (numerator + 1) / (denominator + 1)
+    return 1. - (numerator + smooth) / (denominator + smooth)
