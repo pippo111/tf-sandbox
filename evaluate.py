3af2973fb00dbaa7d95c627ab42cb7835f0beca0
Filip Barańczuk <f.baranczuk@gmail.com>
2019-12-05 06:33:15 -0500

    Fix evaluation in pre-callbacks mode

--- evaluate.py
+++ evaluate.py
@@ -7,12 +7,12 @@ dataset_dir = cfg.setup['dataset_dir']
 valid_loader = get_loader(dataset_dir, 'valid')
 
 for model in cfg.models:
-    my_model = MyModel(
-        batch_size = cfg.setup['batch_size'],
-        checkpoint = model['checkpoint'],
-        valid_loader = valid_loader
-    )
+    my_model = MyModel()
 
-    my_model.load_model(checkpoint = model['checkpoint'], verbose=0)
+    my_model.setup_model(
+        valid_generator=valid_loader,
+        checkpoint=model['checkpoint'])
+
+    my_model.load_model(verbose=0)
 
     my_model.start_evaluate()
